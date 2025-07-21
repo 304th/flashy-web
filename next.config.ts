7b74d960e903845a983241713056948a72e3b04e
avkulpin <a.kulpin@storyfire.com>
2025-07-21 19:47:51 +0300

    Up deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig: NextConfig = {
   output: "export",
+  experimental: {
+    optimizePackageImports: ['firebase', 'framer-motion', '@mux/mux-player-react'],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);

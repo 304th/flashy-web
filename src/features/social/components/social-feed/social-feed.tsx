e6458a3254aab4387daca385e814b37a1d3d0481
avkulpin <a.kulpin@storyfire.com>
2025-09-06 21:22:29 +0300

    Fix loading

--- src/features/social/components/social-feed/social-feed.tsx
+++ src/features/social/components/social-feed/social-feed.tsx
@@ -2,15 +2,18 @@
 
 import { SocialPost } from "@/features/social/components/social-post/social-post";
 import { useSocialPosts } from "@/features/social/queries/useSocialPosts";
+import {Loadable} from "@/components/ui/loadable";
 
 export const SocialFeed = () => {
-  const [socialPosts] = useSocialPosts();
+  const [socialPosts, socialPostsQuery] = useSocialPosts();
 
   return (
     <div className="flex flex-col gap-3">
-      {socialPosts?.map((socialPost) => (
-        <SocialPost key={socialPost._id} socialPost={socialPost} />
-      ))}
+      <Loadable queries={[socialPostsQuery as any]} fullScreenForDefaults>
+        {() => socialPosts?.map((socialPost) => (
+          <SocialPost key={socialPost._id} socialPost={socialPost} />
+        ))}
+      </Loadable>
     </div>
   );
 };

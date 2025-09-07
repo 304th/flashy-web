e3eea6b6c47327c7c36868a3452215df8dfaaa91
avkulpin <a.kulpin@storyfire.com>
2025-09-07 15:24:01 +0300

    Add no posts yet

--- src/features/social/components/social-feed/social-feed.tsx
+++ src/features/social/components/social-feed/social-feed.tsx
@@ -3,6 +3,7 @@
 import { SocialPost } from "@/features/social/components/social-post/social-post";
 import { useSocialPosts } from "@/features/social/queries/useSocialPosts";
 import { Loadable } from "@/components/ui/loadable";
+import {NotFound} from "@/components/ui/not-found";
 
 export const SocialFeed = () => {
   const [socialPosts, socialPostsQuery] = useSocialPosts();
@@ -11,9 +12,9 @@ export const SocialFeed = () => {
     <div className="flex flex-col gap-3">
       <Loadable queries={[socialPostsQuery as any]} fullScreenForDefaults>
         {() =>
-          socialPosts?.map((socialPost) => (
+          socialPosts!.length > 0 ?socialPosts?.map((socialPost) => (
             <SocialPost key={socialPost._id} socialPost={socialPost} />
-          ))
+          )) : <NotFound fullWidth>No posts yet</NotFound>
         }
       </Loadable>
     </div>

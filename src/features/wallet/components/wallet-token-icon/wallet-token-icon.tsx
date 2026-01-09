84ee23a5153daabd36fb720c73a37259275252b2
avkulpin <a.kulpin@storyfire.com>
2026-01-09 10:43:37 +0300

    fix: icon

--- src/features/wallet/components/wallet-token-icon/wallet-token-icon.tsx
+++ src/features/wallet/components/wallet-token-icon/wallet-token-icon.tsx
@@ -1,11 +1,12 @@
 import { BlazeIcon } from "@/components/ui/icons/blaze";
+import { BoltIcon } from "@/components/ui/icons/bolt";
 import { UsdcIcon } from "@/components/ui/icons/usdc";
 import { UsdtIcon } from "@/components/ui/icons/usdt";
 
 export const WalletTokenIcon = ({ token }: { token: WalletToken }) => {
   switch (token) {
     case "blaze":
-      return <BlazeIcon />;
+      return <BoltIcon />;
     case "usdc":
       return <UsdcIcon />;
     case "usdt":
